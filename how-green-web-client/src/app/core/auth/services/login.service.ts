43f5cd161aac5f4279ccbd8531ec37d329108d05
RobotCherries <zdip14@gmail.com>
2022-04-17 00:21:07 +0300

    Added go back link to login page

---
+++ how-green-web-client/src/app/core/auth/services/login.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  public isUserLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  constructor() { }
+}

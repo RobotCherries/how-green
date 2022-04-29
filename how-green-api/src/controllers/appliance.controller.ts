ac09427e46a4ee3e66f9ab0f163518fb1fe74a64
RobotCherries <zdip14@gmail.com>
2022-04-29 14:47:59 +0300

    Finished swagger config for appliance controller

--- how-green-api/src/controllers/appliance.controller.ts
+++ how-green-api/src/controllers/appliance.controller.ts
@@ -1,35 +1,101 @@
 import { Request, Response } from "express";
-import { DeleteResult, getRepository, UpdateResult } from "typeorm";
+import {
+  DeleteResult,
+  getRepository,
+  InsertResult,
+  UpdateResult
+} from "typeorm";
 import { Appliance } from "../entities/appliance.entity";
+import { Project } from './../entities/project.entity';
 
-// Create and Save a new Appliance
+/**
+ * @swagger
+ * /projects/{id}/appliances:
+ *   post:
+ *     description: Use to add a new appliance to a given project (owned by current user)
+ *     produces: ['application/json']
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         type: number
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Appliance name
+ *                 example: Coffee Maker
+ *               description:
+ *                 type: string
+ *                 description: Appliance description
+ *                 example: Bosch coffee maker
+ *               energyClass:
+ *                 type: number
+ *                 description: Project energy class score (from 0 meaning A++, the best energy class to 7 meaning F, the worst energy class)
+ *                 example: 1
+ *               energyConsumptionPerYear:
+ *                 type: number
+ *                 description: Appliance energy consumption per year (kWh/year)
+ *                 example: 125
+ *               wattage:
+ *                 type: number
+ *                 description: Appliance wattage
+ *                 example: 900
+ *     responses:
+ *       201:
+ *         description: Success response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Appliance added successfully
+ *       400:
+ *         description: Invalid projectId, appliance name or energy class
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Invalid projectId, appliance name or energy class
+ *       500:
+ *         description: Server error response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Server error occurred while adding new appliance to given project
+ */
 export const Create = (req: Request, res: Response) => {
   const projectId: number = parseInt(req.params.id);
+  console.log(req.params.id);
 
-  if (!req.params.id) {
-    res.status(400).send({
-      message: "Project Id must be valid!",
+  if (!req.params.id)
+    return res.status(400).send({
+      message: "Invalid project id",
     });
-    return;
-  }
 
-  if (!req.body.name) {
-    res.status(400).send({
-      message: "Name cannot be empty!",
+  if (!req.body.name)
+    return res.status(400).send({
+      message: "Empty appliance name",
     });
-    return;
-  }
 
-  if (
-    req.body.energyClass === null ||
-    req.body.energyClass === "" ||
-    req.body.energyClass === undefined
-  ) {
-    res.status(400).send({
-      message: "Efficiency Class cannot be empty!",
+  if (req.body.energyClass == null || req.body.energyClass === "")
+    return res.status(400).send({
+      message: "Empty energy class",
     });
-    return;
-  }
 
   getRepository(Appliance)
     .createQueryBuilder()
@@ -39,117 +105,364 @@ export const Create = (req: Request, res: Response) => {
       name: req.body.name,
       description: req.body.description,
       energyClass: req.body.energyClass,
-      energyConsumptionPerYear: req.body.energyConsumptionPerYear
-        ? req.body.energyConsumptionPerYear
-        : 0,
-      wattage: req.body.wattage ? req.body.wattage : 0,
+      energyConsumptionPerYear: req.body.energyConsumptionPerYear,
+      wattage: req.body.wattage,
       project: {
         id: projectId,
       },
     })
     .execute()
-    .then((data) => {
-      res.status(201).send(data);
-      console.log("Appliance added successfully!");
+    .then((result: InsertResult) => {
+      res.status(201).send(result);
+      console.log("Appliance added successfully");
     })
-    .catch((err: any) => {
+    .catch((err: Error) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while adding the new appliance.",
+          err.message ||
+          "Server error occurred while adding new appliance to project with id=" +
+            projectId,
       });
     });
 };
 
-export const GetAll = (req: Request, res: Response) => {
-  const projectId = req.params.id;
+/**
+ * @swagger
+ * /projects/{id}/appliances:
+ *   get:
+ *     description: Use to get all appliances from a given project (owned by current user)
+ *     produces: ['application/json']
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         type: number
+ *     responses:
+ *       200:
+ *         description: Success response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Appliance'
+ *       404:
+ *         description: Project not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Could not find given project (owned by current user)
+ *       500:
+ *         description: Server error response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: A server error occurred while retrieving appliances for given project (owned by current user)
+ */
+export const GetAll = async (req: Request, res: Response) => {
+  const projectId: number = parseInt(req.params.id);
+
+  const project = await getRepository(Project).findOne({ where: { id: projectId, userId: req.userId } });
+
+  if(!project)
+    return res.status(404).send({
+      message: "Could not find project with id=" + projectId
+    });
 
   getRepository(Appliance)
-    .find({ where: { project: { id: projectId } } })
-    .then((data: any) => {
-      // console.log("res data", data);
-      res.send(data);
+    .createQueryBuilder()
+    .where("projectId = :projectId", { projectId: projectId })
+    .getMany()
+    .then((appliances: Appliance[]) => {
+      res.status(200).send(appliances);
     })
-    .catch((err: any) => {
+    .catch((err: Error) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while retrieving appliances.",
+          err.message ||
+          `Server error occurred while retrieving appliances for project with id=${projectId}`,
       });
     });
 };
 
-export const GetOne = (req: Request, res: Response) => {
-  const projectId = JSON.parse(req.params.id);
+/**
+ * @swagger
+ * /projects/{id}/appliances/{applianceId}:
+ *   get:
+ *     description: Use to get a given appliance from a given project (owned by current user)
+ *     produces: ['application/json']
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         type: number
+ *       - name: applianceId
+ *         in: path
+ *         required: true
+ *         type: number
+ *     responses:
+ *       200:
+ *         description: Success response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Appliance'
+ *       404:
+ *         description: Project not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Could not find given appliance or project (owned by current user)
+ *       500:
+ *         description: Server error response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: A server error occurred while retrieving given appliance from given project (owned by current user)
+ */
+export const GetOne = async (req: Request, res: Response) => {
+  const projectId: number = parseInt(req.params.id);
   const applianceId = JSON.parse(req.params.applianceId);
 
+  const project = await getRepository(Project).findOne({ where: { id: projectId, userId: req.userId } });
+
+  if(!project)
+    return res.status(404).send({
+      message: "Could not find project with id=" + projectId
+    });
+
   getRepository(Appliance)
-    .findOne(applianceId)
+    .createQueryBuilder()
+    .where("projectId = :projectId", { projectId: projectId })
+    .andWhere("id = :id", { id: applianceId })
+    .getOne()
     .then((data: Appliance | undefined) => {
+      if (!data)
+        return res.status(404).send({
+          message: `Could not find appliance with id=${applianceId} from project with id=${projectId}`,
+        });
 
-      if(data?.projectId !== projectId) {
-        res.status(401).send("You are only allowed to view appliances from your projects.")
-        return;
-      }
-
-      res.send(data);
+      res.status(200).send(data);
     })
-    .catch((err: any) => {
+    .catch((err: Error) => {
       res.status(500).send({
-        message: "Error retrieving Appliance with id=" + applianceId,
+        message:
+          err.message ||
+          `Server error while retrieving appliance with id=${applianceId} from project with id=${projectId}`,
       });
     });
 };
 
-export const Update = (req: Request, res: Response) => {
-  const projectId = JSON.parse(req.params.id);
+/**
+ * @swagger
+ * /projects/{id}/appliances/{applianceId}:
+ *   put:
+ *     description: Use to update a given appliance from a given project (owned by current user)
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         type: number
+ *       - name: applianceId
+ *         in: path
+ *         required: true
+ *         type: number
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Appliance name
+ *                 example: (Updated) Coffee Maker
+ *               description:
+ *                 type: string
+ *                 description: Appliance description
+ *                 example: (Updated) Bosch coffee maker
+ *               energyClass:
+ *                 type: number
+ *                 description: Appliance energy class (from 0 meaning A++, the best energy class to 7 meaning F, the worst energy class)
+ *                 example: 2
+ *               energyConsumptionPerYear:
+ *                 type: number
+ *                 description: Appliance energy consumption per year (kWh/year)
+ *                 example: 125
+ *               wattage:
+ *                 type: number
+ *                 description: Appliance wattage
+ *                 example: 900
+ *     responses:
+ *       200:
+ *         description: Success response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Project updated successfully
+ *       400:
+ *         description: Empty request body
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Could not update project with id=${id} (request body was empty)
+ *       404:
+ *         description: Project or appliance not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Could not find given appliance or project (owned by current user)
+ *       500:
+ *         description: Server error response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Server error while updating given appliance from given project (owned by user)
+ */
+export const Update = async (req: Request, res: Response) => {
+  const projectId: number = parseInt(req.params.id);
   const applianceId = JSON.parse(req.params.applianceId);
 
-  console.log(projectId, req.body);
+  if (!req.body) return res.status(400).send("Empty request body");
 
-  if(req.body.projectId !== projectId) {
-    res.status(401).send("You are only allowed to update appliances from your projects.")
-    return;
-  }
+  const project = await getRepository(Project).findOne({ where: { id: projectId, userId: req.userId } });
+  if(!project)
+    return res.status(404).send({
+      message: "Could not find project with id=" + projectId
+    });
 
   getRepository(Appliance)
     .update(applianceId, req.body)
     .then((result: UpdateResult) => {
       if (result.affected === 1) {
-        res.send({
-          message: "Appliance was updated successfully.",
+        res.status(200).send({
+          message: "Appliance updated successfully",
         });
       } else {
-        res.send({
-          message: `Cannot update Appliance with id=${applianceId}. Maybe Appliance was not found or req.body is empty!`,
+        res.status(404).send({
+          message: `Cannot update appliance with id=${applianceId} from project with id=${projectId} (appliance not found)`,
         });
       }
     })
-    .catch((err: any) => {
+    .catch((err: Error) => {
       res.status(500).send({
-        message: "Error updating Appliance with id=" + applianceId,
+        message:
+          err.message ||
+          `Server error occurred while updating appliance with id=${applianceId} from project with id=${projectId}`,
       });
     });
 };
 
-export const Delete = (req: Request, res: Response) => {
-  const projectId = JSON.parse(req.params.id);
-  const applianceId = JSON.parse(req.params.applianceId);
+/**
+ * @swagger
+ * /projects/{id}/appliances/{applianceId}:
+ *   delete:
+ *     description: Use to delete a given appliance from a given project (owned by current user)
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         type: number
+ *       - name: applianceId
+ *         in: path
+ *         required: true
+ *         type: number
+ *     responses:
+ *       200:
+ *         description: Success response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Appliance deleted successfully
+ *       404:
+ *         description: Project or appliance not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Could not delete given appliance from given project (project or appliance not found)
+ *       500:
+ *         description: Server error response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Server error while deleting project
+ */
+export const Delete = async (req: Request, res: Response) => {
+  const projectId: number = parseInt(req.params.id);
+  const applianceId: number = parseInt(req.params.applianceId);
+
+  const project = await getRepository(Project).findOne({ where: { id: projectId, userId: req.userId } });
+
+  if(!project)
+    return res.status(404).send({
+      message: "Could not find project with id=" + projectId
+    });
 
   getRepository(Appliance)
     .delete(applianceId)
     .then((result: DeleteResult) => {
       if (result.affected === 1) {
-        res.send({
-          message: "Appliance was deleted successfully!",
+        res.status(200).send({
+          message: "Appliance deleted successfully",
         });
       } else {
-        res.send({
-          message: `Cannot delete Appliance with id=${applianceId}. Maybe Appliance was not found!`,
+        res.status(404).send({
+          message: `Could not delete appliance with id=${applianceId} from project with id=${projectId} (appliance not found)`,
         });
       }
     })
-    .catch((err: any) => {
+    .catch((err: Error) => {
       res.status(500).send({
-        message: "Could not delete Appliance with id=" + applianceId,
+        message:
+          err.message ||
+          `Server error while deleting appliance with id=${applianceId} from project with id=${projectId}`,
       });
     });
 };
--- how-green-api/src/entities/appliance.entity.ts
+++ how-green-api/src/entities/appliance.entity.ts
@@ -1,6 +1,46 @@
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Project } from './project.entity';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Appliance:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: Appliance id
+ *           example: 1
+ *         name:
+ *           type: string
+ *           description: Appliance name
+ *           example: Coffee Maker
+ *         description:
+ *           type: string
+ *           description: Appliance description
+ *           example: Bosch coffee maker
+ *         energyClass:
+ *           type: number
+ *           description: Project energy class score (from 0 meaning A++, the best energy class to 7 meaning F, the worst energy class)
+ *           example: 1
+ *         energyConsumptionPerYear:
+ *           type: number
+ *           description: Appliance energy consumption per year (kWh/year)
+ *           example: 125
+ *         wattage:
+ *           type: number
+ *           description: Appliance wattage
+ *           example: 900
+ *         createdAt:
+ *           type: string
+ *           description: Appliance creation date and time
+ *           example: 2022-04-24 12:31:54.439375
+ *         projectId:
+ *           type: integer
+ *           description: Id of the appliance of the project (owned by the current user)
+ *           example: 1
+ */
 @Entity()
 export class Appliance {
     @PrimaryGeneratedColumn()
@@ -16,17 +56,17 @@ export class Appliance {
     @Column()
     name!: string;
 
-    @Column()
-    description?: string;
+    @Column({ nullable: true })
+    description!: string;
 
     @Column()
     energyClass!: number;
 
-    @Column()
-    energyConsumptionPerYear?: number;
+    @Column({ nullable: true })
+    energyConsumptionPerYear!: number;
 
-    @Column()
-    wattage?: number;
+    @Column({ nullable: true })
+    wattage!: number;
 
     @CreateDateColumn()
     createdAt!: Date;
